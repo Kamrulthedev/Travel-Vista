dc2ff153cdf8eca06dd1cd8e04ae73546dfe6937
Kamrul Hassan <kamrulthedev@gmail.com>
2024-10-08 08:17:43 +0600

    create-and-new-update-commit

---
+++ src/app/(WithCommonLayout)/(home)/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HommePage = () => {
+    return (
+        <div>
+            <h1>This is Homme Page</h1>
+        </div>
+    );
+};
+
+export default HommePage;
--- src/app/(commonLayout)/page.tsx
+++
@@ -1,9 +0,0 @@
-const Welcome = () => {
-    return (
-      <div>
-      <h1>Travel Tips</h1>
-      </div>
-    );
-  };
-  
-  export default Welcome;
