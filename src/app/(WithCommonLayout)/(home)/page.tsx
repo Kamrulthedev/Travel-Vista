cb5fff84cc48911c49f253bf5f730175cc503cba
Kamrul Hassan <kamrulthedev@gmail.com>
2024-10-08 22:59:47 +0600

    create-card-commit

--- src/app/(WithCommonLayout)/(home)/page.tsx
+++ src/app/(WithCommonLayout)/(home)/page.tsx
@@ -3,7 +3,7 @@ import Home from "@/components/modules/home/home";
 
 const HommePage = () => {
   return (
-    <div className=" lg:mb-40">
+    <div className="">
       <Home></Home>
     </div>
   );
--- src/components/modules/home/Tread.tsx
+++ src/components/modules/home/Tread.tsx
@@ -1,78 +1,63 @@
-import {Card, CardHeader, CardFooter, Image, Button} from "@nextui-org/react";
-
+import { Card, CardHeader, CardFooter, Image, Button } from "@nextui-org/react";
 
 const Tread = () => {
-    return (
-        <div className="max-w-[900px] gap-2 grid grid-cols-12 grid-rows-2 px-8">
-        <Card className="col-span-12 sm:col-span-4 h-[300px]">
-          <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-            <p className="text-tiny text-white/60 uppercase font-bold">What to watch</p>
-            <h4 className="text-white font-medium text-large">Stream the Acme event</h4>
-          </CardHeader>
-          <Image
-            removeWrapper
-            alt="Card background"
-            className="z-0 w-full h-full object-cover"
-            src="https://nextui.org/images/card-example-4.jpeg"
-          />
-        </Card>
-        <Card className="col-span-12 sm:col-span-4 h-[300px]">
-          <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-            <p className="text-tiny text-white/60 uppercase font-bold">Plant a tree</p>
-            <h4 className="text-white font-medium text-large">Contribute to the planet</h4>
-          </CardHeader>
-          <Image
-            removeWrapper
-            alt="Card background"
-            className="z-0 w-full h-full object-cover"
-            src="https://nextui.org/images/card-example-3.jpeg"
-          />
-        </Card>
-        <Card className="col-span-12 sm:col-span-4 h-[300px]">
-          <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-            <p className="text-tiny text-white/60 uppercase font-bold">Supercharged</p>
-            <h4 className="text-white font-medium text-large">Creates beauty like a beast</h4>
-          </CardHeader>
-          <Image
-            removeWrapper
-            alt="Card background"
-            className="z-0 w-full h-full object-cover"
-            src="https://nextui.org/images/card-example-2.jpeg"
-          />
-        </Card>
-        <Card isFooterBlurred className="w-full h-[300px] col-span-12 sm:col-span-5">
-          <CardHeader className="absolute z-10 top-1 flex-col items-start">
+  return (
+    <div className="bg-white">
+      <div className="pt-16 px-7 text-black space-y-4 pb-3">
+        <h1 className="lg:text-4xl text-2xl font-serif ">
+          Treat yourself to an award-winning meal
+        </h1>
+        <p className="text-base font-serif">
+          2024’s Travelers’ Choice Awards Best of the Best Restaurants
+        </p>
+      </div>
+      {/* Grid Layout for 5 Cards */}
+      <div className="grid md:grid-cols-3 grid-cols-1 lg:grid-cols-4 gap-4 p-5">
+        {/* Card 1 */}
+        <Card isFooterBlurred className=" h-[300px]">
+          <CardHeader className="absolute z-10 top-1 flex-col items-start p-4">
             <p className="text-tiny text-white/60 uppercase font-bold">New</p>
-            <h4 className="text-black font-medium text-2xl">Acme camera</h4>
+            <h4 className="text-white font-medium text-2xl">Cox`s Bazar</h4>
           </CardHeader>
           <Image
             removeWrapper
             alt="Card example background"
             className="z-0 w-full h-full scale-125 -translate-y-6 object-cover"
-            src="https://nextui.org/images/card-example-6.jpeg"
+            src="https://i.ibb.co.com/wyw7z9V/image.png"
           />
-          <CardFooter className="absolute bg-white/30 bottom-0 border-t-1 border-zinc-100/50 z-10 justify-between">
+          <CardFooter className="absolute bg-white/30 bottom-0 border-t-1 border-zinc-100/50 z-10 justify-between p-4">
             <div>
-              <p className="text-black text-tiny">Available soon.</p>
-              <p className="text-black text-tiny">Get notified.</p>
+              <p className="text-white text-tiny">Available soon.</p>
+              <p className="text-white text-tiny">Get notified.</p>
             </div>
-            <Button className="text-tiny" color="primary" radius="full" size="sm">
+            <Button
+              className="text-tiny"
+              color="primary"
+              radius="full"
+              size="sm"
+            >
               Notify Me
             </Button>
           </CardFooter>
         </Card>
-        <Card isFooterBlurred className="w-full h-[300px] col-span-12 sm:col-span-7">
-          <CardHeader className="absolute z-10 top-1 flex-col items-start">
-            <p className="text-tiny text-white/60 uppercase font-bold">Your day your way</p>
-            <h4 className="text-white/90 font-medium text-xl">Your checklist for better sleep</h4>
+
+        {/* Card 2 */}
+        <Card isFooterBlurred className=" h-[300px]">
+          <CardHeader className="absolute z-10 top-1 flex-col items-start p-4">
+            <p className="text-tiny text-white/60 uppercase font-bold">
+              New Location
+            </p>
+            <h4 className="text-white/90 font-medium text-xl">
+               Nill Ghiry, Bandorbon
+            </h4>
           </CardHeader>
           <Image
             removeWrapper
             alt="Relaxing app background"
             className="z-0 w-full h-full object-cover"
-            src="https://nextui.org/images/card-example-5.jpeg"
+            src="https://i.ibb.co.com/zxHn85N/image.png"
           />
-          <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
+          <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100 p-4">
             <div className="flex flex-grow gap-2 items-center">
               <Image
                 alt="Breathing app icon"
@@ -81,14 +66,74 @@ const Tread = () => {
               />
               <div className="flex flex-col">
                 <p className="text-tiny text-white/60">Breathing App</p>
-                <p className="text-tiny text-white/60">Get a good night&lsquo;s sleep.</p>
+                <p className="text-tiny text-white/60">
+                  Get a good night&apos;s sleep.
+                </p>
               </div>
             </div>
-            <Button radius="full" size="sm">Get App</Button>
+            <Button radius="full" size="sm">
+              Get App
+            </Button>
+          </CardFooter>
+        </Card>
+
+        {/* Card 3 */}
+        <Card isFooterBlurred className="w-full h-[300px]">
+          <CardHeader className="absolute z-10 top-1 flex-col items-start p-4">
+            <p className="text-tiny text-white/60 uppercase font-bold">
+              Exclusive
+            </p>
+            <h4 className="text-white font-medium text-2xl">E-nany, Cox`s Bazar</h4>
+          </CardHeader>
+          <Image
+            removeWrapper
+            alt="Smartphone background"
+            className="z-0 w-full h-full object-cover"
+            src="https://i.ibb.co.com/B4mQkg4/image.png"
+          />
+          <CardFooter className="absolute bg-white/30 bottom-0 border-t-1 border-zinc-100/50 z-10 justify-between p-4">
+            <div>
+              <p className="text-black text-tiny">Pre-order now.</p>
+            </div>
+            <Button
+              className="text-tiny"
+              color="primary"
+              radius="full"
+              size="sm"
+            >
+              Pre-order
+            </Button>
+          </CardFooter>
+        </Card>
+
+        {/* Card 4 */}
+        <Card isFooterBlurred className="w-full h-[300px]">
+          <CardHeader className="absolute z-10 top-1 flex-col items-start p-4">
+            <p className="text-tiny text-white/60 uppercase font-bold">
+              Canada
+            </p>
+            <h4 className="text-white/90 font-medium text-xl">
+              I-is-Co Bes, Canada
+            </h4>
+          </CardHeader>
+          <Image
+            removeWrapper
+            alt="Travel background"
+            className="z-0 w-full h-full object-cover"
+            src="https://i.ibb.co.com/9TdWT9F/image.png"
+          />
+          <CardFooter className="absolute flex justify-between bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100 p-4">
+            <div>
+              <p className="text-tiny text-white/60">Breathing App</p>
+            </div>
+            <Button radius="full" size="sm">
+              Learn More
+            </Button>
           </CardFooter>
         </Card>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Tread;
